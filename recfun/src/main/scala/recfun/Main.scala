bc44df7c90b63d70fd1d9772a330de315ea70d44
phtaran <differenceN2@yandex.ru>
2017-02-13 18:37:33 +0200

    Finish first week assignment

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,36 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = if (c == 0 || r == c) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def analyze(rest: List[Char], opened: Int): Boolean = {
+      if (rest.isEmpty) {
+        opened == 0
+      } else {
+        val head = rest.head
+        val n =
+          if (head == '(') opened + 1
+          else if (head == ')') opened - 1
+          else opened
+        if (n >= 0) analyze(rest.tail, n)
+        else false
+      }
+    }
+    analyze(chars, 0)
   }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if ((money < 0) || coins.isEmpty) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
+}
